1fd9c29e066aba36821eae50056689c1f8efb07a
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2023-09-24 22:44:47 +0300

    update

--- internal/control_test_engine/ue/nas/message/nas_control/mm_5gs/context-release-idle.go
+++ internal/control_test_engine/ue/nas/message/nas_control/mm_5gs/context-release-idle.go
@@ -4,14 +4,14 @@ import (
 	"bytes"
 	"fmt"
 	"my5G-RANTester/internal/control_test_engine/ue/context"
-	"my5G-RANTester/internal/control_test_engine/ue/nas/message/nas_control"
+	//"my5G-RANTester/internal/control_test_engine/ue/nas/message/nas_control"
 	"my5G-RANTester/lib/nas"
 	"my5G-RANTester/lib/nas/nasMessage"
 	"my5G-RANTester/lib/nas/nasType"
 )
 
 
-func ContextReleaseIdle(ue *context.UEContext) ([]byte, error) {
+func ContextReleaseIdle(ue *context.UEContext) (nasPdu []byte) {
 
 	m := nas.NewMessage()
 	m.GmmMessage = nas.NewGmmMessage()
@@ -23,11 +23,11 @@ func ContextReleaseIdle(ue *context.UEContext) ([]byte, error) {
 	registrationComplete.SpareHalfOctetAndSecurityHeaderType.SetSpareHalfOctet(0)
 	registrationComplete.RegistrationCompleteMessageIdentity.SetMessageType(nas.MsgTypeRegistrationComplete)
 
-	if sorTransparentContainer != nil {
-		registrationComplete.SORTransparentContainer = nasType.NewSORTransparentContainer(nasMessage.RegistrationCompleteSORTransparentContainerType)
-		registrationComplete.SORTransparentContainer.SetLen(uint16(len(sorTransparentContainer)))
-		registrationComplete.SORTransparentContainer.SetSORContent(sorTransparentContainer)
-	}
+	// if sorTransparentContainer != nil {
+	// 	registrationComplete.SORTransparentContainer = nasType.NewSORTransparentContainer(nasMessage.RegistrationCompleteSORTransparentContainerType)
+	// 	registrationComplete.SORTransparentContainer.SetLen(uint16(len(sorTransparentContainer)))
+	// 	registrationComplete.SORTransparentContainer.SetSORContent(sorTransparentContainer)
+	// }
 
 	m.GmmMessage.RegistrationComplete = registrationComplete
 

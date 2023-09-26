9883d7568211cc2bb99368554318ecc9be7f02fd
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2023-09-26 20:21:28 +0300

    update

--- internal/control_test_engine/gnb/ngap/message/ngap_control/pdu_session_management/pdu-session-resource-release-complete.go
+++ internal/control_test_engine/gnb/ngap/message/ngap_control/pdu_session_management/pdu-session-resource-release-complete.go
@@ -32,7 +32,7 @@ func buildPDUSessionResourceSetupResponseForRegistrationTest(amfUeNgapID, ranUeN
 	successfulOutcome.Criticality.Value = ngapType.CriticalityPresentReject
 
 	successfulOutcome.Value.Present = ngapType.SuccessfulOutcomePresentPDUSessionResourceReleaseResponse
-	successfulOutcome.Value.ProcedureCodePDUSessionResourceRelease = new(ngapType.PDUSessionResourceRelease)
+	successfulOutcome.Value.PDUSessionResourceRelease = new(ngapType.PDUSessionResourceRelease)
 
 	pDUSessionResourceSetupResponse := successfulOutcome.Value.PDUSessionResourceReleaseResponse
 	pDUSessionResourceSetupResponseIEs := &pDUSessionResourceSetupResponse.ProtocolIEs

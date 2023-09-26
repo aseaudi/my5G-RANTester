b5d035cce87c8048d5de935ae5782c7f371f45c2
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2023-09-26 11:45:06 +0300

    update

--- internal/control_test_engine/ue/nas/message/sender/send.go
+++ internal/control_test_engine/ue/nas/message/sender/send.go
@@ -3,10 +3,11 @@ package sender
 import (
 	"fmt"
 	"my5G-RANTester/internal/control_test_engine/ue/context"
+	"encoding/hex"
 )
 
 func SendToGnb(ue *context.UEContext, message []byte) {
-
+	log.Info("[UE] [NAS] UL NAS MSG = ", hex.EncodeToString(message))
 	conn := ue.GetUnixConn()
 	_, err := conn.Write(message)
 	if err != nil {
